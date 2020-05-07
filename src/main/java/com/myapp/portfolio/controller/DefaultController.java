f8811d55f700b3b102f7aae9e05215466050f5db
bjtlr0 <bjtlr@naver.com>
2020-05-07 21:34:20 +0900

    first commit

---
+++ src/main/java/com/myapp/portfolio/controller/DefaultController.java
@@ -0,0 +1,17 @@
+package com.myapp.portfolio.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @작성자 ksh
+ * @Project : portfolio
+ * @FileName : DefaultController.java
+ * @Date : 2020. 5. 7.
+ * @변경이력 : 
+ * @설명 : restful 서버 작성을 위한 테스트 Controller러 작성
+ */
+@RestController
+public class DefaultController {
+
+}
